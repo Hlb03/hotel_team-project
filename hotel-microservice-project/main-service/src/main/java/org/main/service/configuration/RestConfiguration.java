1d6182b69642774ddc6b6505668727f9cdc93742
Hlib <gfietisov@gmail.com>
2023-05-08 10:44:04 +0300

    add rest config to enable microservice communation

---
+++ hotel-microservice-project/main-service/src/main/java/org/main/service/configuration/RestConfiguration.java
@@ -0,0 +1,17 @@
+package org.main.service.configuration;
+
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class RestConfiguration {
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate createRestTemplate(RestTemplateBuilder builder) {
+        return builder.build();
+    }
+}
