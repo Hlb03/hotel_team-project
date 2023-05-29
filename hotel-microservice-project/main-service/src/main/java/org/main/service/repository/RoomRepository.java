9027950762863dba961c7d4eb554292d260cc36e
Hlib <gfietisov@gmail.com>
2023-05-29 23:41:38 +0300

    add query to retrieve info for searching rooms with certain params

--- hotel-microservice-project/main-service/src/main/java/org/main/service/repository/RoomRepository.java
+++ hotel-microservice-project/main-service/src/main/java/org/main/service/repository/RoomRepository.java
@@ -4,8 +4,12 @@ import org.main.service.entity.Room;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.math.BigDecimal;
+import java.sql.Date;
 import java.util.List;
 
 @Repository
@@ -13,4 +17,11 @@ public interface RoomRepository extends JpaRepository<Room, Integer> {
     List<Room> getRoomsByHotelId(int hotelId);
 
     Page<Room> findAll(Pageable pageable);
+
+    @Query("SELECT r FROM Room r LEFT JOIN UserRooms ur ON r.id = ur.room.id " +
+                        "WHERE ((ur.startRent > :startRent AND ur.endRent < :endRent) OR (ur.startRent < :startRent AND ur.startRent < :endRent)) " +
+                                "AND r.price BETWEEN :startPrice AND :endPrice AND r.personAmount >= :personAmount")
+    List<Room> findAllByParams(@Param("startRent") Date startDate, @Param("endRent") Date endDate,
+                               @Param("startPrice") BigDecimal minPrice, @Param("endPrice") BigDecimal maxPrice,
+                               @Param("personAmount") int personAmount);
 }
