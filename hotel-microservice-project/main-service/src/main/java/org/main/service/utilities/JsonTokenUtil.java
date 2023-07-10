156751ecd0c7248c8bf9e525fc9029a83e16d298
Hlib <gfietisov@gmail.com>
2023-07-10 14:06:12 +0300

    add class responsible for token manipulations

---
+++ hotel-microservice-project/main-service/src/main/java/org/main/service/utilities/JsonTokenUtil.java
@@ -0,0 +1,63 @@
+package org.main.service.utilities;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import java.time.Duration;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Component
+public class JsonTokenUtil {
+
+
+    @Value("${jwt.secretkey}")
+    private String secret;
+    @Value("${jwt.lifetime}")
+    private Duration lifetime;
+
+    public String generateToken(UserDetails userDetails) {
+        Date issuedTime = new Date();
+        Date expirationTime = new Date(issuedTime.getTime() + lifetime.toMillis());
+
+        Map<String, Object> claims = new HashMap<>();
+        List<String> authorities = userDetails.getAuthorities()
+                .stream()
+                .map(GrantedAuthority::getAuthority)
+                .toList();
+
+        claims.put("authorities", authorities);
+        System.out.println("TOKEN IS FORMED WITH: " + userDetails);
+
+        return Jwts.builder()
+                .addClaims(claims)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(issuedTime)
+                .setExpiration(expirationTime)
+                .signWith(SignatureAlgorithm.HS256, secret)
+                .compact();
+    }
+
+    public String getUsernameFromToken(String token) {
+        return getTokenClaims(token).getSubject();
+    }
+
+    public List<String> getAuthoritiesFromToken(String token) {
+        return getTokenClaims(token).get("authorities", List.class);
+    }
+
+    private Claims getTokenClaims(String token) {
+        return Jwts
+                .parser()
+                .setSigningKey(secret)
+                .parseClaimsJws(token)
+                .getBody();
+    }
+}
